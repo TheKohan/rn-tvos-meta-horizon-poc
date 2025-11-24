b91f19bda61723e34794a7d14e87db85ad93700a
TheKohan <kohan142011@gmail.com>
2025-11-24 11:48:11 +0100

    init

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Test App!!</Text>
     </View>
   );
 }
